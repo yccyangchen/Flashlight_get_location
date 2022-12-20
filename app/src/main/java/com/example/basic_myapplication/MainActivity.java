57eadc1c7f188853c5f72a19bf8d8de1fad7b4eb
yccyangchen <1193657480@qq.com>
2022-12-20 16:28:58 +0100

    Add existing file

---
+++ app/src/main/java/com/example/basic_myapplication/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.basic_myapplication;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity<SlaphScreen> extends AppCompatActivity {
+
+    private ToggleButton toggleFlashLightOnOff;
+    private CameraManager cameraManager;
+    private String getCameraID;
+
+    private FusedLocationProviderClient fusedLocationClient;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Register the ToggleButton with specific ID
+        toggleFlashLightOnOff = findViewById(R.id.toggle_flashlight);
+
+        // cameraManager to interact with camera devices
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        // Exception is handled, because to check whether
+        // the camera resource is being used by another
+        // service or not.
+        try {
+            // O means back camera unit,
+            // 1 means front camera unit
+            getCameraID = cameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                        }
+                    }
+                });
+
+
+    }
+
+    // RequiresApi is set because, the devices which are
+    // below API level 10 don't have the flash unit with
+    // camera.
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void toggleFlashLight(View view) {
+        if (toggleFlashLightOnOff.isChecked()) {
+            // Exception is handled, because to check
+            // whether the camera resource is being used by
+            // another service or not.
+            try {
+                // true sets the torch in ON mode
+                cameraManager.setTorchMode(getCameraID, true);
+
+                // Inform the user about the flashlight
+                // status using Toast message
+                Toast.makeText(MainActivity.this, "Flashlight is turned ON", Toast.LENGTH_SHORT).show();
+            } catch (CameraAccessException e) {
+                // prints stack trace on standard error
+                // output error stream
+                e.printStackTrace();
+            }
+        } else {
+            // Exception is handled, because to check
+            // whether the camera resource is being used by
+            // another service or not.
+            try {
+                // true sets the torch in OFF mode
+                cameraManager.setTorchMode(getCameraID, false);
+
+                // Inform the user about the flashlight
+                // status using Toast message
+                Toast.makeText(MainActivity.this, "Flashlight is turned OFF", Toast.LENGTH_SHORT).show();
+            } catch (CameraAccessException e) {
+                // prints stack trace on standard error
+                // output error stream
+                e.printStackTrace();
+            }
+        }
+    }
+    // when you click on button and torch open and
+// you do not close the torch again this code
+// will off the torch automatically
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void finish() {
+        super.finish();
+        try {
+            // true sets the torch in OFF mode
+            cameraManager.setTorchMode(getCameraID, false);
+
+            // Inform the user about the flashlight
+            // status using Toast message
+            Toast.makeText(this, "Flashlight is turned OFF", Toast.LENGTH_SHORT).show();
+        } catch (CameraAccessException e) {
+            // prints stack trace on standard error
+            // output error stream
+            e.printStackTrace();
+        }
+    }
+}
